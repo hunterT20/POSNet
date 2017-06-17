02e8322ad0a77c45213b1f3619214b6a48dbff9e
huntert20 <thanhtuan200295@gmail.com>
2017-06-17 12:07:54 +0700

    Initial commit

---
+++ app/src/main/java/com/thanhtuan/posnet/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.thanhtuan.posnet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void LoginClick(){
+        Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+        startActivity(intent);
+    }
+}
